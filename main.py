f76ea0cd45c91aa810031fae11f7b03928039808
MiroshanToshan <abakumovmiron608@gmail.com>
2025-12-21 11:55:05 +0500

    Added all-new interface

--- main.py
+++ main.py
@@ -17,7 +17,120 @@ from config import user_agent,picture_folder_path,document_path,document_folder_
 
 load_dotenv()
 
-url = f''
+
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+MAGENTA = '\033[95m'
+GRAY = '\033[90m'
+CYAN = '\033[96m'
+WHITE = '\033[97m'
+RESET = '\033[0m' 
+BOLD = '\033[1m'
+
+print(f'''
+
+{CYAN}
+╔══════════════════════════════════════════════════════════╗
+║                                                          ║
+║   ██╗    ██╗██╗██╗  ██╗██╗                               ║
+║   ██║    ██║██║██║ ██╔╝██║                               ║
+║   ██║ █╗ ██║██║█████╔╝ ██║                               ║
+║   ██║███╗██║██║██╔═██╗ ██║                               ║
+║   ╚███╔███╔╝██║██║  ██╗██║                               ║
+║    ╚══╝╚══╝ ╚═╝╚═╝  ╚═╝╚═╝                               ║
+║   {BLUE}                                                       ║
+║   {BLUE}██████╗  ██████╗  ██████╗ ███████╗                     ║
+║   {BLUE}██╔══██╗██╔═══██╗██╔════╝ ██╔════╝                     ║
+║   {BLUE}██║  ██║██║   ██║██║      ███████╗                     ║
+║   {BLUE}██║  ██║██║   ██║██║      ╚════██║                     ║
+║   {BLUE}██████╔╝╚██████╔╝╚██████╔╝███████║                     ║
+║   {BLUE}╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝                     ║
+║                                                          ║
+╚══════════════════════════════════════════════════════════╝
+
+{BOLD}{CYAN}This is a script for creating a document from a Wikipedia page. {BLUE}Please, choose your {RED}language:
+    {RED}1. {GREEN}Русский
+    {RED}2. {GREEN}English''')
+
+start_menu = input(f'''{BLUE}-------------------------------------
+{CYAN}Your choice: {RED}''')
+
+if start_menu == '1':
+    print(f'''
+
+
+
+
+
+
+
+{GREEN}Вы выбрали руский язык. {CYAN}Пожалуйста, выберите вариант создания {BLUE}Word-документа:
+
+          {RED}1. {CYAN}С помощью ссылки на статью в Википедии
+          {RED}2. {CYAN}С помощью ключевого слова для статьи в Википедии''')
+    choose_method_ru = input(f'''
+{CYAN}----------------------------------------------
+
+{BLUE}Your choose: {RED}''')
+    if choose_method_ru == '1':
+        url = input(f'''{BLUE}
+--------------------------------------
+{GREEN}Введите ссылку статьи с Википедии: {CYAN}''')
+        print(f'''{CYAN}--------------------------------
+{BLUE}Cоздание документа со статьи: {url}
+{CYAN}----------------------------------------------------{GREEN}''')
+
+
+    elif choose_method_ru == '2':
+        keyword = input(f'''
+{CYAN}-------------------------------------------------------
+{GREEN}Введите ключевое слово: {CYAN}''')
+        print(f'''{CYAN}---------------------------------------
+{BLUE}Cоздание документа с ключевым словом: {keyword}
+{CYAN}---------------------------------------------------------{GREEN}''')
+        url = f'https://ru.wikipedia.org/wiki/{keyword}'
+
+
+
+elif start_menu == '2':
+    print(f'''
+
+
+
+
+
+
+
+{GREEN}You have choosen English language. {CYAN}Please,choose the mode for {BLUE}creating a Word document:
+
+          {RED}1. {CYAN}With a link to Wikipedia article
+          {RED}2. {CYAN}With a keyword for Wikipedia article''')
+    choose_method_ru = input(f'''
+{CYAN}----------------------------------------------
+
+{BLUE}Your choose: {RED}''')
+    if choose_method_ru == '1':
+        url = input(f'''{BLUE}
+--------------------------------------
+{GREEN}Enter the link to the article: {CYAN}''')
+        print(f'''{CYAN}----------------------------------
+{BLUE}Creating a document from article: {url}
+{CYAN}----------------------------------------------------{GREEN}''')
+
+
+    elif choose_method_ru == '2':
+        keyword = input(f'''
+{CYAN}-------------------------------------------------------
+{GREEN}Еnter the article's keyword: {CYAN}''')
+        print(f'''{CYAN}---------------------------------------
+{BLUE}Creating a document with keyword: {keyword}
+{CYAN}---------------------------------------------------------{GREEN}''')
+        url = f'https://en.wikipedia.org/wiki/{keyword}'
+
+
+
 
 create_pictures_folder(picture_folder_path)
 create_document_folder(document_folder_path)
@@ -72,7 +185,20 @@ for index,tag in enumerate(needed_tags):
 
 doc.save(document_path)
 
+if start_menu == '1':
+    print(f'''
+{BLUE}--------------------------------------------
+{GREEN}Документ успешно сохранён в {CYAN}{document_path}
+{BLUE}---------------------------------------------''')
+else:
+    print(f'''
+{BLUE}--------------------------------------------
+{GREEN}Document successfully saved in {CYAN}{document_path}
+{BLUE}---------------------------------------------''')
+
+
 clear_dir(picture_folder_path)
 
 "*th*r*"
+
 
